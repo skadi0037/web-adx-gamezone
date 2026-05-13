b376bff14145aa903175f71d61299487a3e611a0
skadi <1540644301@qq.com>
2026-05-13 17:11:57 +0800

    refactor(analytics, config): 将谷歌分析脚本改为动态加载，移除各页面硬编码的GTM脚本标签

--- js/google/ga.js
+++ js/google/ga.js
@@ -1,7 +1,18 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
+(function () {
+  const GA_ID = "G-9ZDPXP0C33";
 
-gtag("config", "G-9ZDPXP0C33");
+  // 动态加载 gtag.js
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  window.gtag = gtag; // 确保全局可用
+
+  gtag("js", new Date());
+  gtag("config", GA_ID);
+})();
